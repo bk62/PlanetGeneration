620b144bb6f033e74800e1c9792770e74efb3bd3
bk62 <bkafle862@gmail.com>
2019-09-04 21:23:31 +0545

    Added comments

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -7,17 +7,20 @@ public class Planet : MonoBehaviour {
     [Range (2, 256)]
     public int resolution = 10;
 
+    // serialize in order to be able to save
     [SerializeField, HideInInspector]
     MeshFilter[] meshFilters;
     TerrainFace[] terrainFaces;
 
     private void OnValidate () {
+        // update from editor
         Initialize ();
         GenerateMesh ();
     }
 
     void Initialize () {
         if (meshFilters == null || meshFilters.Length == 0) {
+            // only re-initialize if uninitialized
             meshFilters = new MeshFilter[6];
         }
         terrainFaces = new TerrainFace[6];
@@ -25,15 +28,18 @@ public class Planet : MonoBehaviour {
         Vector3[] directions = { Vector3.up, Vector3.down, Vector3.left, Vector3.right, Vector3.forward, Vector3.back };
         
         for (int i = 0; i < 6; i++) {
-            if (meshFilters[i] == null) {
+            if (meshFilters[i] == null) { 
+                // only create mesh if ith mesh filter ininitialized
                 GameObject meshObj = new GameObject ("mesh");
                 meshObj.transform.parent = transform;
 
+                // default material
                 meshObj.AddComponent<MeshRenderer> ().sharedMaterial = new Material (Shader.Find ("Standard"));
                 meshFilters[i] = meshObj.AddComponent<MeshFilter> ();
                 meshFilters[i].sharedMesh = new Mesh ();
             }
 
+            // terrain face in each direction
             terrainFaces[i] = new TerrainFace (meshFilters[i].sharedMesh, resolution, directions[i]);
         }
 
--- Assets/Scripts/TerrainFace.cs
+++ Assets/Scripts/TerrainFace.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TerrainFace {
+    // generate mesh in a given direction
 
     Mesh mesh;
     int resolution;
@@ -20,32 +21,44 @@ public class TerrainFace {
     }
 
     public void ConstructMesh () {
+        // resolution^2 vertices
         Vector3[] vertices = new Vector3[resolution * resolution];
+        // (resolution - 1)^2 squares, 2 triangles for each square and 3 vertices per triangle 
         int[] triangles = new int[(resolution - 1) * (resolution - 1) * 2 * 3];
         int triIndex = 0;
 
         for (int y = 0; y < resolution; y++) {
             for (int x = 0; x < resolution; x++) {
-                int i = x + y * resolution;
+                int i = x + y * resolution; // resolution points per y loop
+                // calc vertex position along the face usinng the percent along the axes from the face origin
                 Vector3 percent = new Vector2 (x, y) / (resolution - 1);
                 Vector3 pointOnUnitCube = localUp + (percent.x - .5f) * 2 * axisA + (percent.y - .5f) * 2 * axisB;
+                // normalized so each vertex is the same distance from planet origin
+                // (uneven distribution of points)
                 Vector3 pointOnUnitSphere = pointOnUnitCube.normalized;
                 vertices[i] = pointOnUnitSphere;
 
+                // skip right and bottom edges
                 if (x != resolution - 1 && y != resolution - 1) {
+                    // triangles defined clockwise
+
+                    // lower triangle in a square  (e.g. ACD if ABCD clockwise square)
                     triangles[triIndex] = i;
                     triangles[triIndex + 1] = i + resolution + 1;
                     triangles[triIndex + 2] = i + resolution;
 
+                    // upper triangle in a square (e.g. ABC if ABCD clockwise square)
                     triangles[triIndex + 3] = i;
                     triangles[triIndex + 4] = i + 1;
                     triangles[triIndex + 5] = i + resolution + 1;
 
-                    triIndex += 6;
+                    triIndex += 6; // 6 triangle vertices added
                 }
             }
         }
 
+        // assigning vertices when lowering resolution causes error b/c some triangle indices are invalid 
+        // so clear mesh first
         mesh.Clear ();
         mesh.vertices = vertices;
         mesh.triangles = triangles;
